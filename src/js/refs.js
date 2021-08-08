3629226585eea6750c5c36aad5762d1698d3061e
ILLIA Kahanov <kahanov.illia@gmail.com>
2021-08-09 00:09:52 +0300

    add theme switch

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,6 @@
 export const refs = {
   toolbar: document.querySelector('.toolbar'),
   themeSwitcher: document.querySelector('#theme-switch-toggle'),
-  menuList: document.querySelector('.js-menu')
+  menuList: document.querySelector('.js-menu'),
+  body: document.querySelector('body')
 }
--- src/js/themeSwitch.js
+++ src/js/themeSwitch.js
@@ -15,15 +15,43 @@
 
 import { refs } from './refs.js';
 
-
 const Theme = {
 LIGHT: 'light-theme',
 DARK: 'dark-theme',
 };
 
-refs.themeSwitcher.addEventListener('change', onThemeChange);
+const onThemeCheck = () => {
+  const savedTheme = localStorage.getItem('theme');
+  if (!savedTheme || savedTheme === Theme.LIGHT) {
+    localStorage.setItem('theme', Theme.LIGHT);
+    refs.body.classList.add(Theme.LIGHT);
+    return
+  }
+  if (savedTheme === Theme.DARK) {
+    refs.themeSwitcher.setAttribute('checked', true);
+    refs.body.classList.add(Theme.DARK);
+    return
+   }  
+}
+
+onThemeCheck();
+
+refs.toolbar.addEventListener('change', onThemeChange);
 
 function onThemeChange(e) {
-
-
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === Theme.LIGHT) {
+    e.target.setAttribute('checked', true);
+    localStorage.setItem('theme', Theme.DARK);
+    refs.body.classList.add(Theme.DARK);
+    refs.body.classList.remove(Theme.LIGHT);
+    return
+  }
+   if (savedTheme === Theme.DARK) {
+    e.target.removeAttribute('checked');
+    localStorage.setItem('theme', Theme.LIGHT);
+     refs.body.classList.add(Theme.LIGHT);
+     refs.body.classList.remove(Theme.DARK);
+     return;
+  }   
 }
